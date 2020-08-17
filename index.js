57c8a795ca2720a3ed6c280336ff7a41ee194e1a
João Miguel Neves <joao@wegotpop.com>
2020-08-17 14:32:09 +0100

    test updated/simplified

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const labels = core.getInput("labels", {required: true}).split(",");
 
 core.setOutput("checkedLabels", labels);
 
-let hasLabel = false;
-labels.foreach(label => github.context.payload.pull_request.labels.some(
-    item => { if (item.name === label.trim()) { hasLabel = true; }}
-));
+const hasLabel = labels.some(
+    label => github.context.payload.pull_request.label.some(
+	item => item.name === label.trim()
+    )
+);
+
 core.setOutput("hasLabel", hasLabel);
 
 console.log(`Does 'pull_request' has at least one of the '${labels}'?: ${hasLabel}`);
