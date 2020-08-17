21f6141c2f68663173248b4103d56120efaee985
João Miguel Neves <joao@wegotpop.com>
2020-08-17 12:47:31 +0100

    update ogs

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ labels.foreach(label => github.context.payload.pull_request.labels.some(
 ));
 core.setOutput("hasLabel", hasLabel);
 
-console.log(`Does 'pull_request' has '${label}' label?: ${hasLabel}`);
+console.log(`Does 'pull_request' has at least one of the '${labels}'?: ${hasLabel}`);
 console.log(`You can use this output as 'steps.<step id>.outputs.hasLabel' and 'steps.<step id>.outputs.checkedLabels' `);
